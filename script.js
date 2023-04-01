2c4cc643f2c0fa52aafc7dda55e942e969920550
Marcos <TheMatdiez@gmail.com>
2023-04-01 00:32:56 -0300

    contact section done + js on the navbar

--- script.js
+++ script.js
@@ -14,3 +14,24 @@ const typed = new Typed('.auto-input', {
     backDelay: 1000,
     loop: true,
 })
+
+//Active Link State on Scroll
+
+//Get all links
+let navLinks = document.querySelectorAll('nav ul li a')
+// Get all sections
+let sections = document.querySelectorAll('section')
+
+window.addEventListener('scroll', function() {
+    const scrollPos = this.window.scrollY + 20
+    sections.forEach(section => {
+        if(scrollPos > section.offsetTop && scrollPos < (section.offsetTop + section.offsetHeight)) {
+            navLinks.forEach(link => {
+                link.classList.remove('active')
+                if(section.getAttribute('id') === link.getAttribute('href').substring(1)) {
+                    link.classList.add('active')
+                }
+            })
+        }
+    })
+})
