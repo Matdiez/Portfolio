bfa354df776d3f902bf8a3ffe34ef923be5bdc9f
Marcos <TheMatdiez@gmail.com>
2023-04-01 03:38:24 -0300

    portfolio-v1 DONE

--- script.js
+++ script.js
@@ -30,31 +30,27 @@ window.addEventListener('scroll', function () {
     })
 })
 
-const btn = document.getElementById('send-msg');
 
-document.getElementById('contact-form').addEventListener('submit', function (event) {
-    event.preventDefault();
 
-    btn.value = 'Sending...';
-
-    const serviceID = 'default_service';
-    const templateID = 'template_yaaugya';
-
-    emailjs.sendForm(serviceID, templateID, this)
-        .then(() => {
-            btn.value = 'Send Email';
-            alert('Sent!');
-        }, (err) => {
-            btn.value = 'Send Email';
-            alert(JSON.stringify(err));
-        });
-    form.reset() 
-});
-
-document.getElementById('contact-form').addEventListener('submit', function (event) {
+document.getElementById('form').addEventListener('submit', function (event) {
     var recaptcha = document.querySelector('[name="g-recaptcha-response"]');
     if (recaptcha.value === '') {
         event.preventDefault();
         alert('Por favor, completa el reCAPTCHA');
     }
+    document.getElementById('form')
+        .addEventListener('submit', function (event) {
+            event.preventDefault();
+
+            const serviceID = 'default_service';
+            const templateID = 'template_hqi1fof';
+
+            emailjs.sendForm(serviceID, templateID, this)
+                .then(() => {
+                    alert('Sent!');
+                }, (err) => {
+                    alert(JSON.stringify(err));
+                });
+            form.reset()
+        });
 });
